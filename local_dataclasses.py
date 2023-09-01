945e9194d45e73305009ae7e42f80fa8c7e8a821
John Scanlan <john.scanlan@gmail.com>
2023-09-01 00:53:34 -0400

    update local data classes and format html for mobile

--- local_dataclasses.py
+++ local_dataclasses.py
@@ -1,7 +1,49 @@
 #!/usr/bin/env python3
+import logging
 from enum import Enum
 from dataclasses import dataclass, field
-from typing import Dict
+from typing import Dict, Set, Tuple, Optional
+
+log = logging.getLogger(__name__)
+
+
+@dataclass
+class StatePixel:
+    state: str = ""
+    name: str = ""
+    y: int = 0
+    x: int = 0
+    b: int = 0
+    g: int = 0
+    r: int = 0
+
+
+@dataclass
+class Point:
+    x: int
+    y: int
+
+
+@dataclass
+class Score:
+    fgreat: int = 0
+    great: int = 0
+    good: int = 0
+    bad: int = 0
+    poor: int = 0
+    fast: int = 0
+    slow: int = 0
+    grade: str = "X"
+
+
+@dataclass
+class MetadataZone:
+    top_left_y: int = 0
+    bottom_right_y: int = 0
+    top_left_x: int = 0
+    bottom_right_x: int = 0
+    state: str = ""
+    area: str = ""
 
 
 class Difficulty(Enum):
@@ -127,3 +169,89 @@ class SongMetadata:
     def sort_by_dpl(self) -> str:
         rate = self.__check_difficulty_rate(self.difficulty[Difficulty.DP_LEGGENDARIA])
         return f"{rate} " + self.sort_by_alphanumeric()
+
+
+@dataclass
+class OCRSongTitles:
+    en_title: str
+    en_artist: str
+    jp_title: str
+    jp_artist: str
+
+
+@dataclass
+class SongReference:
+    by_artist: Dict[str, Set[str]] = field(default_factory=dict)
+    by_difficulty: Dict[Tuple[str, int], Set[str]] = field(default_factory=dict)
+    by_title: Dict[str, str] = field(default_factory=dict)
+
+    def _resolve_artist_ocr(
+        self, song_title: OCRSongTitles, found_difficulty_textage_ids: Set[str]
+    ) -> Optional[str]:
+        found_artist_textage_id = None
+        found_en_artist_textage_ids = self.by_artist.get(song_title.en_artist, set([]))
+        found_jp_artist_textage_ids = self.by_artist.get(song_title.jp_artist, set([]))
+        found_artist_textage_ids = found_en_artist_textage_ids.union(
+            found_jp_artist_textage_ids
+        )
+        if len(found_artist_textage_ids) > 0:
+            matching_ids = found_artist_textage_ids.intersection(
+                found_difficulty_textage_ids
+            )
+            log.info(f"Matching artist/difficulty IDs: {matching_ids}")
+            if len(matching_ids) == 1:
+                found_artist_textage_id = list(matching_ids)[0]
+        return found_artist_textage_id
+
+    def _resolve_title_ocr(
+        self, song_title: OCRSongTitles, found_difficulty_textage_ids: Set[str]
+    ) -> Optional[str]:
+        found_title_textage_id = None
+        found_en_title_textage_id = self.by_title.get(song_title.en_title, None)
+        found_jp_title_textage_id = self.by_title.get(song_title.jp_title, None)
+        log.info(f"found_en_title_textage_id: {found_en_title_textage_id}")
+        log.info(f"found_jp_title_textage_id: {found_jp_title_textage_id}")
+        if found_en_title_textage_id is not None and found_jp_title_textage_id is None:
+            found_title_textage_id = found_en_title_textage_id
+        elif (
+            found_en_title_textage_id is None and found_jp_title_textage_id is not None
+        ):
+            found_title_textage_id = found_jp_title_textage_id
+        elif (
+            found_en_title_textage_id == found_jp_title_textage_id
+            and found_en_title_textage_id is not None
+        ):
+            found_title_textage_id = found_en_title_textage_id
+        return found_title_textage_id
+
+    def resolve_ocr(
+        self, song_title: OCRSongTitles, difficulty: str, level: int
+    ) -> Optional[str]:
+        difficulty_tuple: Tuple[str, int] = (difficulty, level)
+        found_difficulty_textage_ids = self.by_difficulty.get(difficulty_tuple, set([]))
+        if not found_difficulty_textage_ids:
+            log.info(f"Could not lookup difficulty {difficulty_tuple}")
+            return None
+        found_title_textage_id = self._resolve_title_ocr(
+            song_title, found_difficulty_textage_ids
+        )
+        log.info(f"found_title_textage_id: {found_title_textage_id}")
+        if (
+            found_title_textage_id is not None
+            and found_title_textage_id in found_difficulty_textage_ids
+        ):
+            return found_title_textage_id
+        else:
+            log.info(f"Could not find {found_title_textage_id} in difficulties")
+
+        found_artist_textage_id = self._resolve_artist_ocr(
+            song_title, found_difficulty_textage_ids
+        )
+        if found_artist_textage_id is not None:
+            return found_artist_textage_id
+        log.debug("found_title_textage_id: {found_title_textage_id}")
+        log.info(
+            f"Could not find song_title: {song_title} "
+            f"difficulty: {difficulty_tuple}"
+        )
+        return None
--- write_html.py
+++ write_html.py
@@ -15,24 +15,28 @@ def build_table(table_id: Tuple[str, str], songs: List[SongMetadata]) -> str:
         display = "block"
     table_block_start = (
         f"<div id='{table_id[0]}' class='table_area' style='display: {display}'>"
-        f"<h3>{table_id[1]}</h3>\n"
         "<table>\n"
     )
     # TODO: make this column aware to allow for styling/highlighting via css
     table_block_end = f"</table></div>\n"
     header = """
         <tr>
+            <td class='header' colspan=3>Folders</td>
+            <td class='header' colspan=4>Single</td>
+            <td class='header' colspan=4>Double</td>
+        </tr>
+        <tr>
             <td class='header'>Song</td>
             <td class='header'>Version</td>
             <td class='header'>Alpha</td>
-            <td class='header' title='Single Play Normal'>SPN</td>
-            <td class='header' title='Single Play Hyper'>SPH</td>
-            <td class='header' title='Single Play Another'>SPA</td>
-            <td class='header' title='Single Play Leggendaria'>SPL</td>
-            <td class='header' title='Double Play Normal'>DPN</td>
-            <td class='header' title='Double Play Hyper'>DPH</td>
-            <td class='header' title='Double Play Another'>DPA</td>
-            <td class='header' title='Double Play Leggendaria'>DPL</td>
+            <td class='header' title='Single Play Normal'>N</td>
+            <td class='header' title='Single Play Hyper'>H</td>
+            <td class='header' title='Single Play Another'>A</td>
+            <td class='header' title='Single Play Leggendaria'>L</td>
+            <td class='header' title='Double Play Normal'>N</td>
+            <td class='header' title='Double Play Hyper'>H</td>
+            <td class='header' title='Double Play Another'>A</td>
+            <td class='header' title='Double Play Leggendaria'>L</td>
         </tr>
     """
 
@@ -98,6 +102,18 @@ def build_javascript(table_list: List[Tuple[str, str]]) -> str:
                     table.style.display = "none";
                 }
             }
+            var inputs = document.querySelectorAll("input");
+            var clickedInput = "sort_" + visibleTableId;
+            for (const input of inputs) {
+                if(input.id == clickedInput) {
+                    console.log("Clicked " + input.id);
+                    input.style.background = "#000";
+                    input.style.color = "#fff";
+                } else{
+                    console.log("Unclicked " + input.id);
+                    input.removeAttribute("style");
+                }
+            }
             return;
         }
     </script>
@@ -113,20 +129,25 @@ def build_javascript(table_list: List[Tuple[str, str]]) -> str:
 
 def build_buttons(sorted_tables: List[Tuple[str, str]]) -> str:
     buttons = []
-    button_div = "<div id='sort_buttons'>{buttons}</div>"
+    buttons_table = "<table id='sort_buttons'>{buttons}</table>"
     input_template = (
+        "<td>"
         "<input id='sort_{table_id}' "
         "type='button' "
         "value='{table_label}' "
         "onclick='showOneTable(\"{table_id}\")'>"
         "</input>"
+        "</td>"
     )
-    for table_id in sorted_tables:
-        buttons.append(
-            input_template.format(table_id=table_id[0], table_label=table_id[1])
-        )
+    for index, table_id in enumerate(sorted_tables):
+        button = input_template.format(table_id=table_id[0], table_label=table_id[1])
+        if index % 2 == 0:
+            button = "<tr>" + button
+        else:
+            button = button + "</tr>"
+        buttons.append(button)
     button_html = "\n".join(buttons)
-    return button_div.format(buttons=button_html)
+    return buttons_table.format(buttons=button_html)
 
 
 def write_html(sorted_tables: Dict[Tuple[str, str], str]):
@@ -135,32 +156,64 @@ def write_html(sorted_tables: Dict[Tuple[str, str], str]):
     javascript = build_javascript(table_ids)
     buttons = build_buttons(table_ids)
 
-    css = (
-        "td { "
-        "    font-family: sans-serif; "
-        "    border: 1px solid #000000; "
-        "    text-align: center; "
-        "    padding: 0.2em;  "
-        "} "
-        "td.song { text-align: left; min-width: 30ch; }"
-        "div.title { font-size: 1.2em; font-weight: bold; }"
-        "div.genre { font-size: 0.6em; font-weight: bold; }"
-        "div.artist { font-size: 0.6em; font-style: italic;  }"
-        "td.version { min-width:12ch; font-weight: bold; font-size: 0.8em; }"
-        "td.alphanumeric { min-width:8ch; font-weight: bold; font-size: 0.8em; }"
-        "td.spn { min-width:4ch; font-weight: bold;  font-size: 1.4em; }"
-        "td.sph { min-width:4ch; font-weight: bold;  font-size: 1.4em; }"
-        "td.spa { min-width:4ch; font-weight: bold;  font-size: 1.4em; }"
-        "td.spl { min-width:4ch; font-weight: bold;  font-size: 1.4em; }"
-        "td.dpn { min-width:4ch; font-weight: bold;  font-size: 1.4em; }"
-        "td.dph { min-width:4ch; font-weight: bold;  font-size: 1.4em; }"
-        "td.dpa { min-width:4ch; font-weight: bold;  font-size: 1.4em; }"
-        "td.dpl { min-width:4ch; font-weight: bold;  font-size: 1.4em; }"
-        "td.header { background-color: #484848; font-weight: bold; color: #ffffff; }"
-    )
-    # TODO: learn about mobile css
+    css = """
+    @media screen and (max-width:376px) {
+         td {    
+            font-family: sans-serif;    
+             border: 1px solid #000000;     
+            text-align: center;     
+            padding: 0.1em;  
+        }
+
+        table#sort_buttons td { border: none; }
+        table#sort_buttons input { padding: 1ch; }
+        td.song { text-align: left; max-width: 20ch; }
+        div.update { font-size: 0.6em; }
+        div.title { font-size: 0.8em; font-weight: bold; }
+        div.genre { font-size: 0.4em; font-weight: bold; }
+        div.artist { font-size: 0.4em; font-style: italic;  }
+        td.version { min-width:12ch; font-weight: bold; font-size: 0.5em; }
+        td.alphanumeric { min-width:8ch; font-weight: bold; font-size: 0.5em; }
+        td.spn { background: #66FFFF; min-width:2ch; font-weight: bold;  font-size: 0.6em; }
+        td.sph { background: #FFFF45; min-width:2ch; font-weight: bold;  font-size: 0.6em; }
+        td.spa { background: #F89B86; min-width:2ch; font-weight: bold;  font-size: 0.6em; }
+        td.spl { background: #EC98F7; min-width:2ch; font-weight: bold;  font-size: 0.6em; }
+        td.dpn { background: #66FFFF; min-width:2ch; font-weight: bold;  font-size: 0.6em; }
+        td.dph { background: #FFFF45; min-width:2ch; font-weight: bold;  font-size: 0.6em; }
+        td.dpa { background: #F89B86; min-width:2ch; font-weight: bold;  font-size: 0.6em; }
+        td.dpl { background: #EC98F7; min-width:2ch; font-weight: bold;  font-size: 0.6em; }
+        td.header { font-size: 0.6em; background-color: #484848; font-weight: bold; color: #ffffff; }
+           
+        input { min-width: 16ch; margin: 0.6ch;}
+        h3 { font-family: sans-serif; font-weight: bold; }
+        }
+        @media screen and (min-width:376px) {
+        td {     font-family: sans-serif;     border: 1px solid #000000;     text-align: center;     padding: 0.2em;  }
+        table#sort_buttons td { border: none; }
+        table#sort_buttons input { padding: 1ch; }
+        td.song { text-align: left; min-width: 20ch; max-width:50ch; } 
+        div.title { font-size: 1.2em; font-weight: bold; }
+        div.genre { font-size: 0.6em; font-weight: bold; }
+        div.artist { font-size: 0.6em; font-style: italic;  }
+        td.version { min-width:12ch; font-weight: bold; font-size: 0.8em; }
+        td.alphanumeric { min-width:8ch; font-weight: bold; font-size: 0.8em; }
+        td.spn { background: #66FFFF; min-width:2ch; font-weight: bold;  font-size: 1.2em; }
+        td.sph { background: #FFFF45; min-width:2ch; font-weight: bold;  font-size: 1.2em; }
+        td.spa { background: #F89B86; min-width:2ch; font-weight: bold;  font-size: 1.2em; }
+        td.spl { background: #EC98F7; min-width:2ch; font-weight: bold;  font-size: 1.2em; }
+        td.dpn { background: #66FFFF; min-width:2ch; font-weight: bold;  font-size: 1.2em; }
+        td.dph { background: #FFFF45; min-width:2ch; font-weight: bold;  font-size: 1.2em; }
+        td.dpa { background: #F89B86; min-width:2ch; font-weight: bold;  font-size: 1.2em; }
+        td.dpl { background: #EC98F7; min-width:2ch; font-weight: bold;  font-size: 1.2em; }
+        td.header { background-color: #484848; font-weight: bold; color: #ffffff; }
+        input { min-width: 16ch; margin: 0.4ch;}
+        h3 { font-family: sans-serif; font-weight: bold; }
+    }"""
+
     html_template = (
         "<!DOCTYPE html>"
+        "<meta charset='utf-8' />"
+        "<meta name='viewport' content='width=device-width,initial-scale=1' />"
         "<html>"
         "<title>Songs in IIDX Resident Not in Infinitas</title>"
         "<head><style>\n"
@@ -169,8 +222,8 @@ def write_html(sorted_tables: Dict[Tuple[str, str], str]):
         "</style></head>\n"
         "<body>\n"
         "{javascript}"
-        "<div>Generated from <a href='https://textage.cc/score/'>Textage</a> by <a href='https://github.com/everybodyeverybody/textage-data-parser-scripts'>textage-data-parser-scripts</a></div>\n"
-        "<div>Last Update: <b>{utc_now}</b></div>\n"
+        "<div class='update'>Generated from <a href='https://textage.cc/score/'>Textage</a> by <a href='https://github.com/everybodyeverybody/textage-data-parser-scripts'>textage-data-parser-scripts</a></div>\n"
+        "<div class='update'>Last Update: <b>{utc_now}</b></div>\n"
         "{buttons}"
         "{tables}"
         "</body>"
